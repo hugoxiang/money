fc30bca2b54a9a522a37c804d856bffdbf9faa5d
Xiang Zhang <xiang.zhang@essexlg.com>
2021-01-26 23:42:34 +0800

    add hello world test.

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,3 +1,10 @@
-public class MoneyTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MoneyTest {
+    @Test
+    void test_hello_world() {
+        assertTrue(true);
+    }
 }
