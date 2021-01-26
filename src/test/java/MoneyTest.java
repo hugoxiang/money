e16b6f858eaeab995c37343b10c168e326d49039
Xiang Zhang <xiang.zhang@essexlg.com>
2021-01-26 21:04:36 +0800

    [zx] step1 commit.

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,3 +1,14 @@
-public class MoneyTest {
+import com.essexlg.money.Dollar;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTest {
+    @Test
+    void test_multiplication() {
+        final Dollar five = new Dollar(5);
+        five.times(2);
+
+        assertEquals(10, five.amount);
+    }
 }
