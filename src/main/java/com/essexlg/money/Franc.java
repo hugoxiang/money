dee382347784d768b873b99c2d3f28391a7ad500
Xiang Zhang <xiang.zhang@essexlg.com>
2021-01-26 21:45:22 +0800

    step6 commit.

--- src/main/java/com/essexlg/money/Franc.java
+++ src/main/java/com/essexlg/money/Franc.java
@@ -1,19 +1,12 @@
 package com.essexlg.money;
 
-public class Franc {
-    private int amount;
+public class Franc extends Money {
 
     public Franc(int amount) {
-        this.amount = amount;
+        super(amount);
     }
 
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        final Franc dollar = (Franc) o;
-        return amount == dollar.amount;
     }
 }
---
+++ src/main/java/com/essexlg/money/Money.java
@@ -0,0 +1,15 @@
+package com.essexlg.money;
+
+public class Money {
+    protected int amount;
+
+    public Money(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        final Money money = (Money) o;
+        return amount == money.amount;
+    }
+}
