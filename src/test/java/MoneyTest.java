c38c0169e5ce8616f52b19e2bf6f2d975be6ca50
Xiang Zhang <xiang.zhang@essexlg.com>
2021-01-26 21:16:00 +0800

    step2 commit.

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -7,8 +7,11 @@ public class MoneyTest {
     @Test
     void test_multiplication() {
         final Dollar five = new Dollar(5);
-        five.times(2);
+        Dollar product = five.times(2);
 
-        assertEquals(10, five.amount);
+        assertEquals(10, product.amount);
+
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
