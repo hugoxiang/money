e30b655c839eaff9e5a69306a10c2d107b3da608
Xiang Zhang <xiang.zhang@essexlg.com>
2021-01-26 22:19:40 +0800

    step8 commit.

--- src/main/java/com/essexlg/money/Money.java
+++ src/main/java/com/essexlg/money/Money.java
@@ -1,18 +1,20 @@
 package com.essexlg.money;
 
 public abstract class Money {
+    protected final String currency;
     protected int amount;
 
-    public Money(int amount) {
+    public Money(int amount, String currency) {
         this.amount = amount;
+        this.currency = currency;
     }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     @Override
@@ -22,4 +24,8 @@ public abstract class Money {
     }
 
     public abstract Money times(int multiplier);
+
+    public String currency() {
+        return currency;
+    }
 }
